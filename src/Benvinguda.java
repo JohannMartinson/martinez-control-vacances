e372ee4d8584680a3548b843dfa34044067a718b
JohannMartinson <145047795+JohannMartinson@users.noreply.github.com>
2024-11-05 15:35:49 +0100

    Sembla funcionar adecuadament però faltaba fer un comit i push. Aqui està. 20241105-1535

--- src/Benvinguda.java
+++ src/Benvinguda.java
@@ -1,80 +1,188 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+    import javax.swing.*;
+    import java.awt.*;
+    import java.awt.event.*;
+    import java.util.*;
 
-// Aquesta classe representa la finestra de "Benvinguda" on l'usuari pot introduir el seu nom.
-public class Benvinguda extends JFrame {
-    public static final double AMPLADA_PERCENTATGE = 0.5;  // 50% de l'amplada de la pantalla
-    public static final double ALSSADA_PERCENTATGE = 0.8;  // 80% de l'alçada de la pantalla
-    public static final int LOGO_WIDTH = 300;
-    public static final int LOGO_HEIGHT = 225;
-    public static final String TITOL_FINESTRA = "Benvinguda";
-    private JLabel jlImatge;            // Etiqueta per mostrar la imatge.
-    private JTextField jtfNomUsuari;    // Camp de text on l'usuari escriurà el seu nom.
-    private JButton jbEnviar;           // Botó per enviar la informació.
-    private JLabel jlEtiquetaNomUsuari; // Etiqueta per mostrar el text que cal entrar al camp.
+    // Aquesta classe representa la finestra de "Benvinguda" on l'usuari pot introduir el seu nom.
+    public class Benvinguda extends JFrame {
+        public static final double AMPLADA_PERCENTATGE = 0.5;  // 50% de l'amplada de la pantalla
+        public static final double ALSSADA_PERCENTATGE = 0.8;  // 80% de l'alçada de la pantalla
+        public static final int LOGO_WIDTH = 300;
+        public static final int LOGO_HEIGHT = 225;
+        public static final String TITOL_FINESTRA = "Benvinguda";
+        private JLabel jlImatge;            // Etiqueta per mostrar la imatge.
+        private JTextField jtfNomUsuari;    // Camp de text on l'usuari escriurà el seu nom.
+        private JTextField jtfCognomUsuari;    // Camp de text on l'usuari escriurà el seu cognom.
+        private JButton jbEnviar;           // Botó per enviar la informació.
+        private JLabel jlEtiquetaNomUsuari; // Etiqueta per mostrar el text que cal entrar al camp.
+        private JLabel jlEtiquetaCognomUsuari; // Etiqueta per mostrar el text que cal entrar al camp.
+        private JLabel jlEtiquetaDepartament; // Etiqueta per mostrar el text que cal entrar al camp.
+        private JLabel jlEtiquetaAnys;
+        //Combo box
+        private JComboBox<String> comboBox;
 
-    public Benvinguda() {
-        // Obtenir les dimensions de la pantalla
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = toolkit.getScreenSize();
+        private JRadioButton jRadioButton1;
+        private JRadioButton jRadioButton2;
+        private JRadioButton jRadioButton3;
+        private JRadioButton jRadioAnys1;
+        private JRadioButton jRadioAnys2;
+        private JRadioButton jRadioAnys3;
+        private JRadioButton jRadioAnys4;
+        private JRadioButton jRadioAnys5;
+        private JLabel jlTitolApp;
+        private JLabel jlNomEmpresa;
+        private String [] departaments = {"Atenció al client", "Logística", "Gerència"};
 
-        // Calcular la mida de la finestra basada en el percentatge de la mida de la pantalla
-        int amplada = (int) (screenSize.width * AMPLADA_PERCENTATGE);
-        int alssada = (int) (screenSize.height * ALSSADA_PERCENTATGE);
-        double logo_x = (long)Math.floor(amplada /2-LOGO_WIDTH/2);
-        int logo_y = 10;
-        double etiqueta_nom_usuari_x = Math.floor(amplada /2-LOGO_WIDTH/2);
-        double etiqueta_nom_usuari_y =  alssada/2+50;
-        double nom_usuari_x = Math.floor(amplada /2-LOGO_WIDTH/2)+200;
-        double nom_usuari_y = etiqueta_nom_usuari_y;
-        double boto_x = Math.floor(amplada /2-LOGO_WIDTH/2);
-        double boto_y = nom_usuari_y+50;
 
-        // Configuració de la finestra (títol, mida, etc.).
-        setTitle(TITOL_FINESTRA);
-        setSize(amplada, alssada);  // Defineix l'amplada i l'alçada de la finestra relativa a la mida de la pantalla
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Quan es tanca la finestra, es tanca el programa.
-        setLocationRelativeTo(null);  // Centra la finestra a la pantalla.
+        public Benvinguda() {
+            // Obtenir les dimensions de la pantalla
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            Dimension screenSize = toolkit.getScreenSize();
 
-        // Desactivar el layout per defecte i utilitzar null layout
-        setLayout(null);
+            setResizable(false);
 
-        // Crear els components gràfics (camp de text i botó).
-        jlEtiquetaNomUsuari = new JLabel("Introdueix el teu nom:");
-        jtfNomUsuari = new JTextField(20);
-        jbEnviar = new JButton("Enviar");
-        ImageIcon icona = new ImageIcon("src/imatges/nobody_logo.jpg");
-        jlImatge = new JLabel(icona);  // Afegeix la imatge a l'etiqueta
+            // Calcular la mida de la finestra basada en el percentatge de la mida de la pantalla
+            int amplada = (int) (screenSize.width * AMPLADA_PERCENTATGE);
+            int alssada = (int) (screenSize.height * ALSSADA_PERCENTATGE);
+            double logo_x = (long) Math.floor(amplada / 2 - LOGO_WIDTH / 2);
+            int logo_y = 10;
+            double etiqueta_nom_usuari_x = Math.floor(amplada / 2 - LOGO_WIDTH / 2);
+            double etiqueta_nom_usuari_y = alssada / 2 - 50;
+            double nom_usuari_x = Math.floor(amplada / 2 - LOGO_WIDTH / 2) + 200;
+            double nom_usuari_y = etiqueta_nom_usuari_y;
+            double boto_x = (amplada / 3);
+            double boto_y = (alssada - 150);
 
-        // Establir la posició i mida de cada component manualment amb coordenades
-        jlEtiquetaNomUsuari.setBounds((int)etiqueta_nom_usuari_x, (int)etiqueta_nom_usuari_y, 200, 30);
-        jtfNomUsuari.setBounds((int)nom_usuari_x, (int)nom_usuari_y, 150, 30);  // (x, y, width, height)
-        jbEnviar.setBounds((int)boto_x, (int)boto_y, 100, 30);    // Posició i mida del botó
-        jlImatge.setBounds((int)logo_x, logo_y, LOGO_WIDTH, LOGO_HEIGHT);
-        // Afegir els components a la finestra
-        add(jlEtiquetaNomUsuari);
-        add(jtfNomUsuari);
-        add(jbEnviar);
-        add(jlImatge);
+            // Configuració de la finestra (títol, mida, etc.).
+            setTitle(TITOL_FINESTRA);
+            setSize(amplada, alssada);  // Defineix l'amplada i l'alçada de la finestra relativa a la mida de la pantalla
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Quan es tanca la finestra, es tanca el programa.
+            setLocationRelativeTo(null);  // Centra la finestra a la pantalla.
 
-        // Afegir funcionalitat al botó: què passa quan l'usuari fa clic?
-        jbEnviar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Obtenir el text que ha introduït l'usuari al camp de text.
-                String entradaNomUsuari = jtfNomUsuari.getText();
-                if (!entradaNomUsuari.isEmpty()) {
-                    // Si l'usuari ha introduït un nom, obrir la finestra "Resultat" amb aquest nom.
-                    new Resultat(entradaNomUsuari);
-                } else {
-                    // Si el camp de text és buit, mostrar un missatge d'error.
-                    JOptionPane.showMessageDialog(Benvinguda.this,
-                            "Si us plau, cal que introdueixis una cadena.");
+            // Desactivar el layout per defecte i utilitzar null layout
+            setLayout(null);
+
+            // Crear els components gràfics (camp de text i botó).
+            jlEtiquetaNomUsuari = new JLabel("Introdueix el teu nom:");
+            jlEtiquetaCognomUsuari = new JLabel("Introdueix el teu cognom:");
+            jlEtiquetaDepartament = new JLabel("Departament");
+            jlEtiquetaAnys = new JLabel("Anys a l'empresa");
+
+            jtfNomUsuari = new JTextField(20);
+            jtfCognomUsuari = new JTextField(20);
+            jbEnviar = new JButton("Enviar");
+            ImageIcon icona = new ImageIcon("src/imatges/nobody_logo.jpg");
+            jlImatge = new JLabel(icona);  // Afegeix la imatge a l'etiqueta
+            comboBox = new JComboBox<String>();
+
+            //Iniciar els grups on es posen els butons
+            ButtonGroup G = new ButtonGroup();
+            ButtonGroup G2 = new ButtonGroup();
+
+            // Iniciar els botons per el departament
+            jRadioButton1 = new JRadioButton(departaments[0]);
+            jRadioButton2 = new JRadioButton(departaments[1]);
+            jRadioButton3 = new JRadioButton(departaments[2]);
+
+            // Iniciar els botons per l'any
+            jRadioAnys1 = new JRadioButton("Menys de un any");
+            jRadioAnys2 = new JRadioButton("Un any");
+            jRadioAnys3 = new JRadioButton("Dos anys");
+            jRadioAnys4 = new JRadioButton("Tres anys");
+            jRadioAnys5 = new JRadioButton("Més de tres anys");
+
+            jlTitolApp = new JLabel("Sistema de Control de Vacances");
+            jlNomEmpresa = new JLabel("2024 - Nobody S.A.");
+
+            // Establir la posició i mida de cada component manualment amb coordenades
+            jlEtiquetaNomUsuari.setBounds((int) etiqueta_nom_usuari_x, (int) etiqueta_nom_usuari_y, 200, 30);
+            jlEtiquetaCognomUsuari.setBounds((int) etiqueta_nom_usuari_x, (int) etiqueta_nom_usuari_y+35, 200, 30);
+            jlEtiquetaDepartament.setBounds((int) etiqueta_nom_usuari_x, (int) etiqueta_nom_usuari_y+75, 200, 30);
+            jlEtiquetaAnys.setBounds((int) etiqueta_nom_usuari_x+200, (int) etiqueta_nom_usuari_y+75, 200, 30);
+            jlNomEmpresa.setBounds((int) amplada-20, (int) alssada-10, 200, 30);
+            jlTitolApp.setBounds((int) etiqueta_nom_usuari_x, (int) etiqueta_nom_usuari_y - 50, 200, 30);
+            jtfNomUsuari.setBounds((int) nom_usuari_x, (int) nom_usuari_y, 150, 30);  // (x, y, width, height)
+            jtfCognomUsuari.setBounds((int) nom_usuari_x, (int) nom_usuari_y+30, 150, 30);  // (x, y, width, height)
+            jbEnviar.setBounds((int) boto_x, (int) boto_y, 100, 30);    // Posició i mida del botó
+            jlImatge.setBounds((int) logo_x, logo_y, LOGO_WIDTH, LOGO_HEIGHT);
+            jRadioButton1.setBounds((int) etiqueta_nom_usuari_x, (int) etiqueta_nom_usuari_y + 100, 200, 30);
+            jRadioButton2.setBounds((int) etiqueta_nom_usuari_x, (int) etiqueta_nom_usuari_y + 125, 200, 30);
+            jRadioButton3.setBounds((int) etiqueta_nom_usuari_x, (int) etiqueta_nom_usuari_y + 150, 200, 30);
+            //jRadioAnys1.setBounds((int) etiqueta_nom_usuari_x+200, (int) etiqueta_nom_usuari_y + 100, 200, 30);
+            //jRadioAnys2.setBounds((int) etiqueta_nom_usuari_x+200, (int) etiqueta_nom_usuari_y + 125, 200, 30);
+            //jRadioAnys3.setBounds((int) etiqueta_nom_usuari_x+200, (int) etiqueta_nom_usuari_y + 150, 200, 30);
+            //jRadioAnys4.setBounds((int) etiqueta_nom_usuari_x+200, (int) etiqueta_nom_usuari_y + 175, 200, 30);
+            //jRadioAnys5.setBounds((int) etiqueta_nom_usuari_x+200, (int) etiqueta_nom_usuari_y + 200, 200, 30);
+            comboBox.setBounds((int) etiqueta_nom_usuari_x+200, (int) etiqueta_nom_usuari_y+75, 200, 30);
+
+            add(comboBox);
+
+            comboBox.addItem("menys de un any");
+            comboBox.addItem("un any");
+            comboBox.addItem("de dos a sis anys");
+            comboBox.addItem("set o més anys");
+
+            // Afegir els components a la finestra
+            add(jlEtiquetaNomUsuari);
+            add(jlEtiquetaCognomUsuari);
+            add(jlEtiquetaDepartament);
+            add(jlEtiquetaAnys);
+            add(jlNomEmpresa);
+            add(jlTitolApp);
+            add(jtfNomUsuari);
+            add(jtfCognomUsuari);
+            add(jbEnviar);
+            add(jlImatge);
+            G.add(jRadioButton1);
+            G.add(jRadioButton2);
+            G.add(jRadioButton3);
+            add(jRadioButton1);
+            add(jRadioButton2);
+            add(jRadioButton3);
+
+            // Afegir funcionalitat al botó: què passa quan l'usuari fa clic?
+            jbEnviar.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+
+                    String entradaDepartament = "";
+                    //Obtenir els anys seleccionats en el desplegable
+                    String anysTreballats = (String) comboBox.getSelectedItem();
+                    //Obtenir el valor introduit en el JFT Nom
+                    String nomTreballador = jtfNomUsuari.getText();
+
+                    // Obtenir el text que ha introduït l'usuari al camp de text.
+                    if (jRadioButton1.isSelected()) {
+                        entradaDepartament = jRadioButton1.getText();
+                        //Comprovació per pantalla que la variable es guarda correctmaent
+                        //JOptionPane.showMessageDialog(Benvinguda.this,entradaDepartament);
+                    } else if (jRadioButton2.isSelected()) {
+                        entradaDepartament = jRadioButton2.getText();
+                    } else if (jRadioButton3.isSelected()) {
+                        entradaDepartament = jRadioButton3.getText();
+                    }
+                    //Comprovar si totes les dades han estat introduides
+                    if (anysTreballats!= null && !entradaDepartament.isEmpty() && !nomTreballador.isEmpty())  {
+                        // Si l'usuari ha introduït un nom, obrir la finestra "Resultat" amb aquest nom.
+                        new Resultat(nomTreballador, entradaDepartament, anysTreballats);
+                    } else {
+                        //Mostrant missatge d'error si falta el nom
+                        if(nomTreballador.isEmpty()){
+                            // Si el camp de text és buit, mostrar un missatge d'error
+                            JOptionPane.showMessageDialog(Benvinguda.this,
+                                    "Si us plau, cal que introdueixis un nom.");
+
+                        }
+                        //Mostrant missatges d'error si falta el departament
+                        if(entradaDepartament.isEmpty()){
+                            // Si el departament no ha estat triat
+                            JOptionPane.showMessageDialog(Benvinguda.this,
+                                    "Cal que triïs un departament.");
+                        }
+                    }
                 }
-            }
-        });
+            });
 
-        // Mostrar la finestra.
-        setVisible(true);
+            // Mostrar la finestra.
+            setVisible(true);
+        }
     }
-}
--- src/Resultat.java
+++ src/Resultat.java
@@ -1,28 +1,57 @@
 import javax.swing.*;
-import java.time.LocalTime;
-
-import eines.Eines;  // Importar la classe Eines del paquet eines.
 
 // Aquesta classe representa la finestra que mostra la salutació personalitzada.
 public class Resultat extends JFrame {
 
-    public Resultat(String text) {
+    public Resultat(String nom_treballador, String departament, String anys_treballats) {
         // Configuració de la finestra
         setTitle("Resultat");
-        setSize(300, 150);  // Defineix l'amplada i l'alçada de la finestra.
+        setSize(700, 200);  // Defineix l'amplada i l'alçada de la finestra.
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);  // Tanca només aquesta finestra, no tot el programa.
         setLocationRelativeTo(null);  // Centra la finestra a la pantalla.
 
-        // Obtenir la salutació correcta utilitzant el mètode de la classe Eines
-        String salutacio = Eines.obtenirSalutacio();
+        // Inicialitzar la variable per guardar la frase del resultat
+        String frase_resultat = calcularDiesVacances(nom_treballador, departament, anys_treballats);
 
         // Crear el missatge amb la salutació correcta i el nom de l'usuari.
-        JLabel label = new JLabel(salutacio + text, SwingConstants.CENTER);  // Col·locar el text centrat.
+        JLabel label = new JLabel(frase_resultat, SwingConstants.CENTER);  // Col·locar el text centrat.
 
         // Per afegir el missatge a la finestra.
         add(label);
 
         // Mostrar la finestra.
         setVisible(true);
+    }
+
+    private String calcularDiesVacances(String nom_treballador, String departament, String anys_treballats) {
+        int diesVacances = 0;
+
+        // Comprovació del departament escollit
+        if (departament.equals("Atenció al client")) {
+            if (anys_treballats.equals("un any") || anys_treballats.equals("menys de un any")) {
+                diesVacances = 6;
+            } else if (anys_treballats.equals("de dos a sis anys")) {
+                diesVacances = 14;
+            } else if (anys_treballats.equals("set o més anys")) {
+                diesVacances = 20;
+            }
+        } else if (departament.equals("Logística")) {
+            if (anys_treballats.equals("un any") || anys_treballats.equals("menys de un any")) {
+                diesVacances = 7;
+            } else if (anys_treballats.equals("de dos a sis anys")) {
+                diesVacances = 15;
+            } else if (anys_treballats.equals("set o més anys")) {
+                diesVacances = 22;
+            }
+        } else if (departament.equals("Gerència")) {
+            if (anys_treballats.equals("un any") || anys_treballats.equals("menys de un any")) {
+                diesVacances = 10;
+            } else if (anys_treballats.equals("de dos a sis anys")) {
+                diesVacances = 20;
+            } else if (anys_treballats.equals("set o més anys")) {
+                diesVacances = 30;
+            }
+        }
+        return "En " + nom_treballador + " de " + departament + " amb " + anys_treballats + " a l'empresa,  mereix " + diesVacances + " dies de vacances.";
     }
 }
